0bd4671bdf8b26edae0e3d479a81836414fded1d
Carmen <carmencheney99@gmail.com>
2024-08-13 19:03:53 -0400

    finished

--- index.js
+++ index.js
@@ -39,12 +39,16 @@ const mainMenu = () => {
                 viewEmployees();
                 break;
             case 'Add a department':
+                addDepartment();
                 break;
             case 'Add a role':
+                addRole();
                 break;
             case 'Add an employee':
+                addEmployee();
                 break;
             case 'Update an employee role':
+                updateRole();
                 break;
             case 'Seed Database':
                 seedDatabase();
@@ -77,11 +81,157 @@ const viewRoles = () => {
 };
 
 const viewEmployees = () => {
-    client.query('SELECT * FROM employee', (err, res) => {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department,
+                    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+                    FROM employee INNER JOIN
+                    role ON employee.role_id = role.id INNER JOIN
+                    department ON role.department_id = department.id LEFT JOIN
+                    employee manager ON employee.manager_id = manager.id`;
+            
+    client.query(query, (err, res) => {
         if (err) throw err;
         console.table(res.rows);
         mainMenu();
     });
+};
+
+const addDepartment = () => {
+    inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'add department here'
+    }).then(answer => {
+        client.query('INSERT INTO department (name) VALUES ($1)', [answer.name], (err, res) => {
+            if (err) throw err;
+            console.log(`Department ${answer.name} added`);
+            mainMenu();
+        });
+    });
+}
+
+const addRole = () => {
+    client.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        const departments = res.rows.map(d => ({
+            name: d.name,
+            value: d.id
+        }));
+       inquirer.prompt([
+           {
+               name: 'title',
+               type: 'input',
+               message: 'Enter title:'
+            },
+            {
+                name: 'salary',
+                type: 'number',
+                message: 'Enter salary:'
+            },
+            {
+                name: 'department_id',
+                type: 'list',
+                message: 'Select department:',
+                choices: departments
+            }
+        ]).then(answer => {
+            client.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [answer.title, answer.salary, answer.department_id], (err, res) => {
+                if (err) throw err;
+                console.log(`Role ${answer.title} added`);
+                mainMenu();
+            });
+        });        
+    });
+};
+
+const addEmployee = () => {
+    const query = `SELECT role.id, role.title, role.salary, department.name AS department
+                   FROM role
+                   JOIN department ON role.department_id = department.id`;
+    client.query(query, (err, res) => {
+        if (err) throw err;
+        const roles = res.rows.map(d => ({
+            name: `${d.title} - ${d.department}`,
+            value: d.id
+        }));
+        client.query('SELECT * FROM employee', (err, res) => {
+            if (err) throw err;
+            const employees = res.rows.map(e => ({
+                name: `${e.first_name} ${e.last_name}`,
+                value: e.id
+        }));
+        employees.push({ name: 'None', value: 0 });
+       inquirer.prompt([
+           {
+               name: 'first_name',
+               type: 'input',
+               message: 'Enter first name:'
+            },
+            {
+                name: 'last_name',
+                type: 'input',
+                message: 'Enter last name:'
+            },
+            {
+                name: 'role_id',
+                type: 'list',
+                message: 'Select role:',
+                choices: roles
+            },
+            {
+                name: 'manager_id',
+                type: 'list',
+                message: 'Select manager:',
+                choices: employees
+            }
+        ]).then(answer => {
+            client.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [answer.first_name, answer.last_name, answer.role_id, answer.manager_id], (err, res) => {
+                if (err) throw err;
+                console.log(`Employee ${answer.first_name} ${answer.last_name} added`);
+                mainMenu();
+            });
+        });        
+    });
+});
+};
+
+const updateRole = () => {
+    const query = `SELECT role.id, role.title, role.salary, department.name AS department
+                   FROM role
+                   JOIN department ON role.department_id = department.id`;
+    client.query(query, (err, res) => {
+        if (err) throw err;
+        const roles = res.rows.map(d => ({
+            name: `${d.title} - ${d.department}`,
+            value: d.id
+        }));
+        client.query('SELECT * FROM employee', (err, res) => {
+            if (err) throw err;
+            const employees = res.rows.map(e => ({
+                name: `${e.first_name} ${e.last_name}`,
+                value: e.id
+        }));
+       inquirer.prompt([
+           {
+               name: 'employee_id',
+               type: 'list',
+               message: 'Select employee:',
+               choices: employees
+            },
+            {
+                name: 'role_id',
+                type: 'list',
+                message: 'Select role:',
+                choices: roles
+            }
+        ]).then(answer => {
+            client.query('UPDATE employee SET role_id = $1 WHERE id = $2', [answer.role_id, answer.employee_id], (err, res) => {
+                if (err) throw err;
+                console.log(`Employee role updated`);
+                mainMenu();
+            });
+        });        
+    });
+});
 };
 
 function seedDatabase() {
